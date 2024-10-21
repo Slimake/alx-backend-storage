e1bae7026345372e38d17a8a0b12b4a5d45f1231
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-21 20:40:44 +0100

    List all bands with Glam rock

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,20 @@
+-- 3-glam_rock
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+CREATE VIEW metal_bands_view AS
+SELECT 
+    band_name,
+    CASE
+        WHEN split IS NULL THEN 2022 - formed
+        ELSE split - formed 
+    END AS lifespan
+FROM 
+    metal_bands
+WHERE 
+    style LIKE '%Glam rock%';
+
+SELECT 
+    band_name, lifespan
+FROM 
+    metal_bands_view
+ORDER BY 
+    lifespan DESC;
