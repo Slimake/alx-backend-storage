974e243fbf099416c65e2500f60f5f9edb0d7c7f
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-10-23 03:48:12 +0100

    Change all topics of a school document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""10-update_topic"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    """
+    mongo_collection.update(
+            {"name": name},
+            {"$set": {"topics": topics}})
